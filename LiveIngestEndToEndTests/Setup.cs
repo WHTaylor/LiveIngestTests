b2a6ed3026246cbc0298d11447f0b8a60e6e64a2
Will Taylor <william.h.taylor@stfc.ac.uk>
2021-07-30 17:27:42 +0100

    Config example

--- LiveIngestEndToEndTests/Setup.cs
+++ LiveIngestEndToEndTests/Setup.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
 
 namespace LiveIngestEndToEndTests
 {
@@ -8,11 +12,32 @@ namespace LiveIngestEndToEndTests
         [OneTimeSetUp]
         public void EnvironmentSetup()
         {
+            var config = new ConfigurationBuilder()
+                .AddIniFile("appsettings.ini")
+                .Build();
+
+            var exes = config
+                .GetSection("executables")
+                .GetChildren()
+                .Select(c => c.Value);
+
+            foreach (var exe in exes)
+            {
+                if (File.Exists(exe)) continue;
+                SetupError($"Required exe {exe} does not exist");
+            }
+
             // Validate things are running/available to run
             var toWatch = new DataArchiverCreator().CreateDataArchive();
             // Setup ICAT data? AKA delete leftovers in advance
             // Queue component setup/maybe clearing?
             // Startup live ingest process(es)
+        }
+
+        private void SetupError(string msg)
+        {
+            Console.Error.WriteLine($"Fatal error during setup: {msg}");
+            Assert.Fail();
         }
     }
 }
