e5bf34caee22161ccd5735149034c16d7dcc99b3
Will Taylor <william.h.taylor@stfc.ac.uk>
2021-09-02 11:55:23 +0100

    Rename asserter

---
+++ LiveIngestEndToEndTests/Framework/MessageAsserter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ICAT4IngestLibrary.ActiveMQ;
+using NUnit.Framework;
+
+namespace LiveIngestEndToEndTests.Framework
+{
+    /// <summary>
+    /// Methods to assert messages reach the expected queues.
+    ///
+    /// Most commonly, one of these assertions should be the first in a test
+    /// because they poll the expected queue, which accounts for the async
+    /// nature of the ingest apps.
+    /// </summary>
+    public class MessageAsserter
+    {
+        private readonly AMQClient _amqClient =
+            new(Environment.GetEnvironmentVariable("AMQ_BROKER_URL"), true);
+
+        public void ReachesSuccess(
+            string filename,
+            int timeout = 15,
+            int pollingIntervalMs = 3000)
+        {
+            // Note: NUnit has a bug that means the value delegate is called
+            // twice on each poll interval, so don't do anything which relies
+            // on only happening once https://github.com/nunit/nunit/issues/2841
+            var constraint = Is.True
+                .After(timeout).Seconds
+                .PollEvery(pollingIntervalMs).MilliSeconds;
+            var failMsg = $"{filename} failed to reach success queue within " +
+                $"{timeout} seconds";
+            Assert.That(() => InSuccessQueue(filename), constraint, failMsg);
+        }
+
+        private bool InSuccessQueue(string fileName)
+        {
+            return _amqClient.IsInQueue(fileName, MessageQueue.ICATSuccess);
+        }
+    }
+}
--- LiveIngestEndToEndTests/Tests/SomeTest.cs
+++ LiveIngestEndToEndTests/Tests/SomeTest.cs
@@ -8,34 +8,20 @@ namespace LiveIngestEndToEndTests.Tests
     public class Tests
     {
         private readonly DataFileCopier _dataFileCopier = new("EMU");
-        private readonly DelayedAssert _asserter = new();
+        private readonly MessageAsserter _messageAsserter = new();
         private static readonly ICATClient _icatClient = Framework.ICAT.Client;
-
-        [SetUp]
-        public void Setup()
-        {
-            // Set up any log/queue following stuff if that seems like a good idea
-            var i = _icatClient.Service.search(
-                _icatClient.SessionId,
-                "select i from Investigation i where i.name =  'CAL_EMU_21/04/2021 11:42:06' include 1");
-            if (i?.Length > 0)
-            {
-                TestContext.Progress.WriteLine(
-                    "No matter what the asserter may think, inv was created. Deleting it");
-                _icatClient.Service.delete(_icatClient.SessionId, (entityBaseBean) i[0]);
-            }
-        }
+        private const string _expectedFileName = "CAL_EMU_21/04/2021 11:42:06";
 
         [TearDown]
         public void TearDown()
         {
             var i = _icatClient.Service.search(
                 _icatClient.SessionId,
-                "select i from Investigation i where i.name =  'CAL_EMU_21/04/2021 11:42:06' include 1");
+                $"select i from Investigation i " +
+                $"where i.name =  '{_expectedFileName}' include 1");
+
             if (i?.Length > 0)
             {
-                TestContext.Progress.WriteLine(
-                    "No matter what the asserter may think, inv was created. Deleting it");
                 _icatClient.Service.delete(_icatClient.SessionId, (entityBaseBean) i[0]);
             }
         }
@@ -44,7 +30,13 @@ namespace LiveIngestEndToEndTests.Tests
         public void Test1()
         {
             _dataFileCopier.CopyRun("112486");
-            _asserter.Success("112486.xml");
+            _messageAsserter.ReachesSuccess("112486.xml");
+            var i = _icatClient.Service.search(
+                _icatClient.SessionId,
+                $"select i from Investigation i " +
+                $"where i.name =  '{_expectedFileName}' include 1");
+            Assert.AreEqual(1, i?.Length,
+                $"The expected file '{_expectedFileName}' was not in ICAT");
         }
     }
 }
